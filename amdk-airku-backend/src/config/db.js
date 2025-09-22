0d0b7b9fd93060b94b22c37051e4ff7d1a7cbf8b
Seva <99525875+Tenshi-X@users.noreply.github.com>
2025-09-22 22:41:08 +0700

    Update db.js

--- amdk-airku-backend/src/config/db.js
+++ amdk-airku-backend/src/config/db.js
@@ -3,17 +3,18 @@ require('dotenv').config();
 
 const dbUrl = new URL(process.env.DATABASE_URL);
 
-const pool = mysql.createPool({
-  host: dbUrl.hostname,
-  user: dbUrl.username,
-  password: dbUrl.password,
-  database: dbUrl.pathname.substring(1),
-  port: dbUrl.port,
+const dbConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   dateStrings: true
-});
+};
 
+const pool = mysql.createPool(dbConfig);
 // Export the promise-based pool for async/await
 module.exports = pool.promise();
